fd782605412614eb81a407b7197ecb59269ef3b8
Aternus <kiril@atern.us>
2023-10-30 11:18:33 -0400

    FEAT: refactor - improved handling of quick commits (no prefix and task)

--- generate-pr-description.sh
+++ generate-pr-description.sh
@@ -78,8 +78,7 @@ echo "$commit_messages" | while read -r line; do
       output_to_file "$prefix: $message"
     fi
   # case: - detail - another detail
-  # case: did something quick via GitHub UI
-  elif [[ "$line" =~ ^- || ! "$line" =~ ^: ]]; then
+  elif [[ "$line" =~ ^- ]]; then
     IFS='-' read -ra split_details <<<"$line"
     # Process details
     for detail in "${split_details[@]}"; do
@@ -88,6 +87,11 @@ echo "$commit_messages" | while read -r line; do
         output_to_file "CHORE: other - $processed_detail"
       fi
     done
+  # case: did something quick via GitHub UI
+  # case: updated package-name
+  elif [[ ! "$line" =~ ^: ]]; then
+    processed_detail=$(trim "$line")
+    output_to_file "CHORE: other - $processed_detail"
   fi
 done
 
